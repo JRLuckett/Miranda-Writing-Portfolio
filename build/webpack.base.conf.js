cf77d037fc53a56d6a269a01e15cec394d4cff4e
JONATHON LUCKETT <jluckettt@gmail.com>
2017-10-20 14:29:37 -0600

    revert

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,7 +22,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/boom/vue.esm.js',
+      'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
     }
   },
